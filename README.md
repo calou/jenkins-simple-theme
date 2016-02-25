5feb2259308dbeb827c42bc16f1e71d89e8b290a
Sébastien Gruchet <gruchet@gmail.com>
2016-02-25 21:36:57 +0100

    Removing FontAwesome

---
+++ README.md
@@ -0,0 +1,2 @@
+# Theme for Jenkins
+Requires [Simple theme plugin](https://wiki.jenkins-ci.org/display/JENKINS/Simple+Theme+Plugin)
--- theme.js
+++ theme.js
@@ -1,16 +1,10 @@
 
 setTimeout(function(){
-	var iconClocks = document.getElementsByClassName("icon-clock");
-	for(var i in iconClocks){
-		if( "length" != i && "item" != i && "namedItem" != i ){
-			var iconClock = $(iconClocks[i]);
-			var clock = document.createElement("span");
-			clock.className = "clock fa fa-flag-checkered";
-			clock.onclick = iconClock.onclick;
-			iconClock.insertAdjacentElement("beforeBegin", clock);
-		}
-	}
-	
+    /*
+	insertSpanBefore("icon-clock", "faicon clock fa fa-flag-checkered");
+	insertSpanBefore("icon-red", "faicon fa fa-exclamation-circle");
+	insertSpanBefore("icon-blue", "faicon fa fa-check-circle");
+
 	var addTabs = document.getElementsByClassName("addTab");
 	for(var i in addTabs){
 		console.log(i, $(addTabs[i]));
@@ -19,4 +13,25 @@ setTimeout(function(){
 			addTab.innerHTML = "<span class='fa fa-plus-square'></span>";
 		}
 	}
-}, 200);
+	*/
+
+	var jenkinsNameIcon = document.getElementById("jenkins-name-icon");
+	var titleSpan = document.createElement("span");
+	titleSpan.innerHTML = "Jenkins"
+	titleSpan.style="font-family:'Lobster', cursive; font-size:28px; color:#FFFFFF; text-decoration:none;"
+	jenkinsNameIcon.insertAdjacentElement("beforeBegin", titleSpan);
+
+}, 100);
+
+function insertSpanBefore(className, newClassName) {
+	var icons = document.getElementsByClassName(className);
+	for (var i in icons){
+		if( "length" != i && "item" != i && "namedItem" != i ){
+			var icon = $(icons[i]);
+			var faIcon = document.createElement("span");
+			faIcon.className = newClassName;
+			faIcon.onclick = icon.onclick;
+			icon.insertAdjacentElement("beforeBegin", faIcon);
+		}
+	}
+}
