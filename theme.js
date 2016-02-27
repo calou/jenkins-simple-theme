6bcb1f6877890781e73e619a63033bb651475294
Sébastien Gruchet <gruchet@gmail.com>
2016-02-27 07:54:18 +0100

    Added images

--- theme.js
+++
@@ -1,9 +0,0 @@
-
-setTimeout(function(){
-	var jenkinsNameIcon = document.getElementById("jenkins-name-icon");
-	var titleSpan = document.createElement("span");
-	titleSpan.innerHTML = "Jenkins"
-	titleSpan.style="font-family:'Lobster', cursive; font-size:28px; color:#FFFFFF; text-decoration:none;"
-	jenkinsNameIcon.insertAdjacentElement("beforeBegin", titleSpan);
-}, 100);
-
